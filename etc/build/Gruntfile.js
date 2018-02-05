0b5197fa8e9cc957dcde09875000a5f60ce90af9
mehyaa <mehyaa@gmail.com>
2018-02-06 00:47:05 +0300

    Module generation mechanism changed
    When generating modules, ability to use modules seperately added.

--- etc/build/Gruntfile.js
+++ etc/build/Gruntfile.js
@@ -1,79 +1,207 @@
-var path = require("path"),
-    fs = require("fs"),
-    parentFolderName = path.basename(path.resolve('..')),
-    mxClientContent,
-    deps;
+const decomment = require("decomment");
+const fs = require("fs");
+const path = require("path");
 
-module.exports = function (grunt) {
-    // To get the dependencies for the project, read the filenames by matching
-    // mxClient.include([...]) in mxClient.js. This is not perfect, but the list is
-    // required in mxClient.js for compatibility.
-    mxClientContent = fs.readFileSync(
-        path.join(process.cwd(), "./javascript/src/js/mxClient.js"),
-        "utf8"
-    );
-    deps = mxClientContent.match(/mxClient\.include\([^"']+["'](.*?)["']/gi).map(function (str) {
-        return "." + str.match(/mxClient\.include\([^"']+["'](.*?)["']/)[1];
-    });
-    deps = ["./js/mxClient.js"].concat(deps.slice(0));
+const flatten = arr =>
+  arr.reduce(
+    (acc, val) => acc.concat(Array.isArray(val) ? flatten(val) : val),
+    []
+  );
+
+Array.prototype.flatten = function() {
+  return flatten(this);
+};
+
+const walkSync = dir =>
+  fs
+    .readdirSync(dir)
+    .map(
+      file =>
+        fs.statSync(path.join(dir, file)).isDirectory()
+          ? walkSync(path.join(dir, file))
+          : path.join(dir, file).replace(/\\/g, "/")
+    )
+    .flatten();
+
+const regFilter = (arr, filter) =>
+  arr.filter(item =>
+    (filter instanceof RegExp ? filter : new RegExp(filter, "gi")).test(item)
+  );
+
+const parentFolderName = path.basename(path.resolve(".."));
+
+module.exports = function(grunt) {
+  const files = regFilter(
+    walkSync(path.join(process.cwd(), "javascript/src/js")),
+    /\.js$/
+  );
+  const modules = files.map(file => path.basename(file, ".js"));
+  const subModules = {
+    mxCellAttributeChange: "mxGraphModel",
+    mxChildChange: "mxGraphModel",
+    mxCollapseChange: "mxGraphModel",
+    mxCurrentRootChange: "mxGraphView",
+    mxGeometryChange: "mxGraphModel",
+    mxHierarchicalEdgeStyle: "mxHierarchicalLayout",
+    mxRootChange: "mxGraphModel",
+    mxSelectionChange: "mxGraphSelectionModel",
+    mxStyleChange: "mxGraphModel",
+    mxTerminalChange: "mxGraphModel",
+    mxValueChange: "mxGraphModel",
+    mxVisibleChange: "mxGraphModel"
+  };
+  const nonModules = ["mxDisabled", "mxPlainTextEditor"];
+  const undefinedModules = [];
+
+  let distContent = `
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define([], factory);
+  }
+  else if (typeof module === 'object' && module.exports) {
+    module.exports = factory();
+  }
+  else {
+    root.mxgraph = factory();
+  }
+} (this, function () {
+  return function (opts) {
+    var options = require('./getOptions')(opts);
+
+    var mxNamespace = {};
+
+    mxNamespace.mxClient = require('./mxClient')(options, true);
+`;
 
   grunt.initConfig({
+    clean: ["./javascript/dist"],
     copy: {
       main: {
-        files: [{
-          expand: true,
-          cwd: "./javascript/src",
-          src: deps,
-          dest: "./javascript/dist"
-        }],
+        files: [
+          {
+            expand: true,
+            cwd: "./javascript/src",
+            src: "**/*.js",
+            dest: "./javascript/dist",
+            flatten: true
+          }
+        ],
         options: {
-          // After each module, add the object to the '__mxOutput' namespace
-          // E.g. __mxOutput.mxLog, etc.
-          process: function (content, srcpath) {
-            var afterContent = "",
-                moduleName = path.basename(srcpath, ".js");
+          process: function(content, srcpath) {
+            const moduleName = path.basename(srcpath, ".js");
+            const isCodecDefinition =
+              moduleName !== "mxCodec" &&
+              moduleName !== "mxObjectCodec" &&
+              moduleName.endsWith("Codec");
+            const isMxClient = moduleName === "mxClient";
 
-            afterContent += "\n__mxOutput." + path.basename(srcpath, ".js") +
-              " = typeof " + moduleName + " !== 'undefined' ? " + moduleName + " : undefined;\n";
+            if (isMxClient) {
+              content = content.replace(
+                /mxClient\.include\([^"']+["'](.*?)["']\);/gi,
+                ""
+              );
+            }
 
-            return content + afterContent;
+            const cleanContent = decomment(content);
+
+            const selfDefined = cleanContent.match(new RegExp(moduleName, "g"))
+              ? true
+              : false;
+
+            if (isCodecDefinition && !selfDefined) {
+              content = content.replace(
+                /mxCodecRegistry\.register\(function\(\)/g,
+                "var " + moduleName + " = mxCodecRegistry.register(function()"
+              );
+            }
+
+            const extraDefinitionsMatch = cleanContent.match(
+              /(function\s(mx[A-Z]\w+)|var\s(mx[A-Z]\w+))/g
+            );
+
+            const extraDefinitions = (extraDefinitionsMatch || [])
+              .map(value => value.replace(/(function\s|var\s)/gi, ""))
+              .filter(
+                (value, index, self) =>
+                  value !== moduleName && self.indexOf(value) === index
+              );
+
+            const moduleUndefinedModules = cleanContent
+              .match(/\smx[A-Z]\w+/g)
+              .map(value => value.trim())
+              .filter(
+                (value, index, self) =>
+                  value !== moduleName &&
+                  !modules.includes(value) &&
+                  !subModules[value] &&
+                  self.indexOf(value) === index
+              );
+
+            if (moduleUndefinedModules && moduleUndefinedModules.length) {
+              moduleUndefinedModules
+                .sort()
+                .forEach(
+                  value =>
+                    !undefinedModules.includes(value) &&
+                    undefinedModules.push(value)
+                );
+              // console.log(moduleName, " => ", moduleUndefinedModules.join(", "));
+            }
+
+            const requires = cleanContent
+              .match(/\smx[A-Z]\w+/g)
+              .map(value => value.trim())
+              .filter(
+                (value, index, self) =>
+                  value !== moduleName &&
+                  (modules.includes(value) || subModules[value]) &&
+                  self.indexOf(value) === index
+              )
+              .sort()
+              .map(value => {
+                const req = subModules[value] || value;
+                const sub = subModules[value] ? "." + value : "";
+                return `var ${value} = require('./${req}')(options, true)${sub};`;
+              })
+              .join("\n");
+
+            if (!isMxClient) {
+              distContent += `\n    mxNamespace.${moduleName} = require('./${moduleName}')(options, true);`;
+              extraDefinitions.forEach(
+                value =>
+                  (distContent += `\n    mxNamespace.${value} = mxNamespace.${moduleName}.${value};`)
+              );
+            }
+
+            return `module.exports = function ${moduleName}Factory(opts, merged) {
+var options = merged ? (opts || {}) : require('./getOptions')(opts);
+
+${requires}
+
+for (var name in Object.keys(options)) {
+  this[name] = options[name];
+}
+
+${content}
+
+${extraDefinitions
+  .map(value => `${moduleName}.${value} = ${value};`)
+  .join("\n")}
+
+return ${moduleName};
+};`;
           }
         }
-      }
-    },
-    concat: {
-      dist: {
-        src: deps.map(function (dep) {
-          return path.join("./javascript/dist", dep);
-        }),
-        dest: './javascript/dist/build.js'
-      },
-      options: {
-        banner: "(function (root, factory) {\n" +
-          "if (typeof define === 'function' && define.amd) {\n" +
-          "define([], factory);\n" +
-          "} else if (typeof module === 'object' && module.exports) {\n" +
-          "module.exports = factory();\n" +
-          "} else {\n" +
-          "root.mxgraph = factory();\n" +
-          "}\n" +
-          "}(this, function () {\n" +
-          "return function (opts) {\n" +
-          // Opts will be passed into this function, expand them out as if
-          // they were globals so they can get picked up by the logic in
-          // mxClient.js.
-          "for (var name in opts) { this[name] = opts[name]; }\n" +
-          "var __mxOutput = {};\n",
-        footer: "return __mxOutput;\n" +
-          "};\n" +
-          "}));"
       }
     },
     webpack: {
       examples: {
         entry: "./javascript/examples/webpack/src/anchors.js",
         output: {
-            path: path.resolve(process.cwd(), "./javascript/examples/webpack/dist"),
+          path: path.resolve(
+            process.cwd(),
+            "./javascript/examples/webpack/dist"
+          ),
           filename: "anchors.js"
         }
       }
@@ -86,16 +214,48 @@ module.exports = function (grunt) {
           interrupt: true
         }
       }
-    },
+    }
   });
 
-  require(parentFolderName === "node_modules" ? "load-grunt-parent-tasks" : "load-grunt-tasks")(grunt);
-  grunt.registerTask("default", [
-    "copy",
-    "concat",
-    "webpack"
-  ]);
-  grunt.registerTask("build", [
-    "default"
-  ]);
+  require(parentFolderName === "node_modules"
+    ? "load-grunt-parent-tasks"
+    : "load-grunt-tasks")(grunt);
+
+  grunt.registerTask("finalize", "", function finalize(opts) {
+    distContent += `
+
+    return mxNamespace;
+  };
+}))`;
+    fs.writeFileSync(
+      path.join(process.cwd(), "javascript/dist/build.js"),
+      distContent
+    );
+    fs.writeFileSync(
+      path.join(process.cwd(), "javascript/dist/mxDefaultOptions.js"),
+      `module.exports = function mxDefaultOptionsFactory() {
+  return {
+    mxLoadResources: true,
+    mxForceIncludes: false,
+    mxResourceExtension: '.txt',
+    mxLoadStylesheets: true,
+    mxBasePath: '.',
+    mxImageBasePath: './images',
+    mxLanguage: null,
+    mxDefaultLanguage: 'en',
+    mxLanguages: ['en']
+  };
+};`
+    );
+    fs.writeFileSync(
+      path.join(process.cwd(), "javascript/dist/getOptions.js"),
+      `module.exports = function getOptions(options) {
+  return Object.assign(require('./mxDefaultOptions')() || {}, options || {});
+};`
+    );
+    // console.log('Undefined module(s): ', undefinedModules.sort().join(', '));
+  });
+
+  grunt.registerTask("default", ["clean", "copy", "finalize", "webpack"]);
+  grunt.registerTask("build", ["default"]);
 };
